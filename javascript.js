b1280b7d75c56473b2302446875a506c525ef8e5
Doomand56 <doomand56@gmail.com>
2022-04-12 22:50:01 +0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,41 @@
+// Вычесление суммы в степени
+
+document.querySelector('.button').onclick = function () {
+	let inpOne = +document.querySelector('.inp-1').value;
+	let inpTwo = +document.querySelector('.inp-2').value;
+	let outOne = document.querySelector('.out-1');
+
+	function stepen (a, b) {
+		if (b == 1) {
+			return a;
+		}
+		else {
+			return a = a * stepen(a, b - 1);
+		}
+	}
+	outOne.innerHTML = stepen(inpOne, inpTwo);
+};
+
+
+// Фейдер
+
+document.querySelector('.inp-range').oninput = function () {
+	document.querySelector('.out-2').innerHTML = this.value;
+}
+
+
+// Счётчик случайных чисел
+
+document.querySelector('.button-2').onclick = function () {
+	function timer () {
+	function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	let spanOne = document.querySelector('.span-1');
+	spanOne.innerHTML = getRandomIntInclusive(0, 100);
+	}
+	setInterval(timer, 1000);
+	this.setAttribute("disabled", "disabled");
+}
